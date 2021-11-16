4854237e5af965bcb212ad8c8cc575dbaca7599e
Park Chul Hyun <85903711+ironpark1105@users.noreply.github.com>
2021-11-16 21:32:59 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -9,7 +9,6 @@ document.addEventListener('scroll', () => {
     }
 }); 
 
-
 // Handle scrolling when tapping on the navbar menu
 const navbarMenu = document.querySelector('.navbar__menu');
 navbarMenu.addEventListener('click', (event) => {
